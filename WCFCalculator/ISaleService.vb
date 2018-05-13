596156001d0495947f93ffc34a735bef04485082
syndiperr <aj.abundez2012@gmail.com>
2018-05-13 12:15:16 -0500

    Configured and initialized WCFService to use functions for gathering products, invoices, & customers

---
+++ WCFCalculator/ISaleService.vb
@@ -0,0 +1,38 @@
+﻿' NOTE: You can use the "Rename" command on the context menu to change the interface name "IService1" in both code and config file together.
+<ServiceContract()>
+Public Interface ISaleService
+
+    <OperationContract()>
+    Function GetProduct(ByVal _productID As Integer) As Product
+
+    <OperationContract()>
+    Function ListProducts() As List(Of Product)
+
+    <OperationContract()>
+    Function ListInvoices() As List(Of Invoice)
+
+    <OperationContract()>
+    Function GetInvoice(ByVal _customerID As String) As Invoice
+
+    <OperationContract()>
+    Function CreateInvoice(ByVal _invoice As Invoice) As Boolean
+
+    <OperationContract()>
+    Function GetCustomer(ByVal _customerID As String) As Customer
+
+End Interface
+
+<DataContract()>
+Public Class Product
+
+End Class
+
+<DataContract()>
+Public Class Customer
+
+End Class
+
+<DataContract()>
+Public Class Invoice
+
+End Class
---
+++ WCFCalculator/SaleService.vb
@@ -0,0 +1,28 @@
+﻿' NOTE: You can use the "Rename" command on the context menu to change the class name "Service1" in both code and config file together.
+Public Class SaleService
+    Implements ISaleService
+
+    Public Function GetProduct(_productID As Integer) As Product Implements ISaleService.GetProduct
+        Throw New NotImplementedException()
+    End Function
+
+    Public Function ListProducts() As List(Of Product) Implements ISaleService.ListProducts
+        Throw New NotImplementedException()
+    End Function
+
+    Public Function ListInvoices() As List(Of Invoice) Implements ISaleService.ListInvoices
+        Throw New NotImplementedException()
+    End Function
+
+    Public Function GetInvoice(_customerID As String) As Invoice Implements ISaleService.GetInvoice
+        Throw New NotImplementedException()
+    End Function
+
+    Public Function CreateInvoice(_invoice As Invoice) As Boolean Implements ISaleService.CreateInvoice
+        Throw New NotImplementedException()
+    End Function
+
+    Public Function GetCustomer(_customerID As String) As Customer Implements ISaleService.GetCustomer
+        Throw New NotImplementedException()
+    End Function
+End Class
